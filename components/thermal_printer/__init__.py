60c2e4cbdc73e6944575ccb3f5611ffb1f617327
Jeroen ter Heerdt <jterh@microsoft.com>
2025-04-20 18:08:30 +0000

    first version of print_image

--- components/thermal_printer/__init__.py
+++ components/thermal_printer/__init__.py
@@ -1,1 +1,8 @@
-DEPENDENCIES = ["image"]
+import esphome.codegen as cg
+
+CODEOWNERS = ["@jeroenterheerdt"]
+DEPENDENCIES = ["uart", "image"]
+MULTI_CONF = True
+
+thermal_printer = cg.esphome_ns.namespace("thermal_printer")
+ThermalPrinter = thermal_printer.class_("ThermalPrinter", cg.Component)
