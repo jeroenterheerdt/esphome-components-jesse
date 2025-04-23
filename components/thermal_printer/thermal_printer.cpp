faa2c8cdf3f50516b5bbf2be7da1e1fcdd3d2d61
Jeroen ter Heerdt <jterh@microsoft.com>
2025-04-23 02:58:50 +0000

    trying once more to print esphome logo

--- components/thermal_printer/thermal_printer.cpp
+++ components/thermal_printer/thermal_printer.cpp
@@ -497,10 +497,10 @@ void ThermalPrinterDisplay::print_image(std::string image, int width) {
 }
 
 bool ThermalPrinterDisplay::has_paper() {
-  this->write_array(GET_STATUS_CMD);
+  this->write_array(GET_STATUS_CMD, sizeof(GET_STATUS_CMD));
 
   int status = -1;
-  uint8_t *data = -1;
+  uint8_t *data = nullptr;
   for (uint8_t i = 0; i < 10; i++) {
     this->read_byte(data);
     if (data != nullptr) {
