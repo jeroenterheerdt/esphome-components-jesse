1573ebced644bd2d0c783c830cae52fc5396a29f
Jeroen ter Heerdt <jeroen_ter_heerdt@hotmail.com>
2026-01-21 03:35:11 +0000

    more methods and fixes

--- components/m5stack_printer/display.py
+++ components/m5stack_printer/display.py
@@ -16,6 +16,18 @@ M5StackPrinterPrintTextAction = m5stack_printer_ns.class_(
     "M5StackPrinterPrintTextAction", automation.Action
 )
 
+M5StackPrinterPrintQRCodeAction = m5stack_printer_ns.class_(
+    "M5StackPrinterPrintQRCodeAction", automation.Action
+)
+
+M5StackPrinterPrintBarcodeAction = m5stack_printer_ns.class_(
+    "M5StackPrinterPrintBarcodeAction", automation.Action
+)
+
+M5StackPrinterNewLineAction = m5stack_printer_ns.class_(
+    "M5StackPrinterNewLineAction", automation.Action
+)
+
 # Additional action classes for new features
 M5StackPrinterCutPaperAction = m5stack_printer_ns.class_(
     "M5StackPrinterCutPaperAction", automation.Action
@@ -29,6 +41,38 @@ M5StackPrinterSetStyleAction = m5stack_printer_ns.class_(
     "M5StackPrinterSetStyleAction", automation.Action
 )
 
+M5StackPrinterSet90DegreeRotationAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSet90DegreeRotationAction", automation.Action
+)
+
+M5StackPrinterSetInversePrintingAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSetInversePrintingAction", automation.Action
+)
+
+M5StackPrinterSetChineseModeAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSetChineseModeAction", automation.Action
+)
+
+M5StackPrinterSetLineSpacingAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSetLineSpacingAction", automation.Action
+)
+
+M5StackPrinterSetPrintDensityAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSetPrintDensityAction", automation.Action
+)
+
+M5StackPrinterSetTabPositionsAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSetTabPositionsAction", automation.Action
+)
+
+M5StackPrinterHorizontalTabAction = m5stack_printer_ns.class_(
+    "M5StackPrinterHorizontalTabAction", automation.Action
+)
+
+M5StackPrinterSetHorizontalPositionAction = m5stack_printer_ns.class_(
+    "M5StackPrinterSetHorizontalPositionAction", automation.Action
+)
+
 M5StackPrinterPrintTestPageAction = m5stack_printer_ns.class_(
     "M5StackPrinterPrintTestPageAction", automation.Action
 )
@@ -39,13 +83,23 @@ M5StackPrinterRunDemoAction = m5stack_printer_ns.class_(
 
 CONF_FONT_SIZE = "font_size"
 CONF_TEXT = "text"
+CONF_QRCODE = "qrcode"
+CONF_BARCODE = "barcode"
+CONF_TYPE = "type"
+CONF_LINES = "lines"
 CONF_CUT_MODE = "cut_mode"
-CONF_FEED_LINES = "feed_lines" 
+CONF_FEED_LINES = "feed_lines"
 CONF_ALIGNMENT = "alignment"
 CONF_BOLD = "bold"
 CONF_UNDERLINE = "underline"
 CONF_DOUBLE_WIDTH = "double_width"
 CONF_UPSIDE_DOWN = "upside_down"
+CONF_ENABLE = "enable"
+CONF_SPACING = "spacing"
+CONF_DENSITY = "density"
+CONF_BREAK_TIME = "break_time"
+CONF_POSITIONS = "positions"
+CONF_POSITION = "position"
 CONF_SHOW_QR_CODE = "show_qr_code"
 CONF_SHOW_BARCODE = "show_barcode"
 CONF_SHOW_TEXT_STYLES = "show_text_styles"
@@ -109,17 +163,254 @@ async def m5stack_printer_print_text_action_to_code(
 
 
 @automation.register_action(
+    "m5stack_printer.print_qrcode",
+    M5StackPrinterPrintQRCodeAction,
+    cv.maybe_simple_value(
+        cv.Schema(
+            {
+                cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+                cv.Required(CONF_QRCODE): cv.templatable(cv.string),
+            }
+        ),
+        key=CONF_QRCODE,
+    ),
+)
+async def m5stack_printer_print_qrcode_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_QRCODE], args, cg.std_string)
+    cg.add(var.set_qrcode(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.print_barcode",
+    M5StackPrinterPrintBarcodeAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_BARCODE): cv.templatable(cv.string),
+            cv.Required(CONF_TYPE): cv.templatable(cv.string),
+        }
+    ),
+)
+async def m5stack_printer_print_barcode_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_BARCODE], args, cg.std_string)
+    cg.add(var.set_barcode(templ))
+    templ = await cg.templatable(config[CONF_TYPE], args, cg.std_string)
+    cg.add(var.set_type(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.new_line",
+    M5StackPrinterNewLineAction,
+    cv.maybe_simple_value(
+        cv.Schema(
+            {
+                cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+                cv.Required(CONF_LINES): cv.templatable(cv.int_range(min=1, max=255)),
+            }
+        ),
+        key=CONF_LINES,
+    ),
+)
+async def m5stack_printer_new_line_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_LINES], args, cg.uint8)
+    cg.add(var.set_lines(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_90_degree_rotation",
+    M5StackPrinterSet90DegreeRotationAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_ENABLE): cv.templatable(cv.boolean),
+        }
+    ),
+)
+async def m5stack_printer_set_90_degree_rotation_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_ENABLE], args, cg.bool_)
+    cg.add(var.set_enable(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_inverse_printing",
+    M5StackPrinterSetInversePrintingAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_ENABLE): cv.templatable(cv.boolean),
+        }
+    ),
+)
+async def m5stack_printer_set_inverse_printing_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_ENABLE], args, cg.bool_)
+    cg.add(var.set_enable(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_chinese_mode",
+    M5StackPrinterSetChineseModeAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_ENABLE): cv.templatable(cv.boolean),
+        }
+    ),
+)
+async def m5stack_printer_set_chinese_mode_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_ENABLE], args, cg.bool_)
+    cg.add(var.set_enable(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_line_spacing",
+    M5StackPrinterSetLineSpacingAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_SPACING): cv.templatable(cv.int_range(min=0, max=255)),
+        }
+    ),
+)
+async def m5stack_printer_set_line_spacing_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_SPACING], args, cg.uint8)
+    cg.add(var.set_spacing(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_print_density",
+    M5StackPrinterSetPrintDensityAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_DENSITY): cv.templatable(cv.int_range(min=0, max=31)),
+            cv.Optional(CONF_BREAK_TIME, default=0): cv.templatable(
+                cv.int_range(min=0, max=7)
+            ),
+        }
+    ),
+)
+async def m5stack_printer_set_print_density_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_DENSITY], args, cg.uint8)
+    cg.add(var.set_density(templ))
+    templ = await cg.templatable(config[CONF_BREAK_TIME], args, cg.uint8)
+    cg.add(var.set_break_time(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_tab_positions",
+    M5StackPrinterSetTabPositionsAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_POSITIONS): cv.templatable(cv.string),
+        }
+    ),
+)
+async def m5stack_printer_set_tab_positions_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_POSITIONS], args, cg.std_string)
+    cg.add(var.set_positions(templ))
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.horizontal_tab",
+    M5StackPrinterHorizontalTabAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+        }
+    ),
+)
+async def m5stack_printer_horizontal_tab_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    return var
+
+
+@automation.register_action(
+    "m5stack_printer.set_horizontal_position",
+    M5StackPrinterSetHorizontalPositionAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
+            cv.Required(CONF_POSITION): cv.templatable(cv.int_range(min=0, max=383)),
+        }
+    ),
+)
+async def m5stack_printer_set_horizontal_position_action_to_code(
+    config, action_id, template_arg, args
+):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    templ = await cg.templatable(config[CONF_POSITION], args, cg.uint16)
+    cg.add(var.set_position(templ))
+    return var
+
+
+@automation.register_action(
     "m5stack_printer.cut_paper",
     M5StackPrinterCutPaperAction,
     cv.Schema(
         {
             cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
-            cv.Optional(CONF_CUT_MODE, default=0): cv.templatable(cv.int_range(min=0, max=66)),
-            cv.Optional(CONF_FEED_LINES, default=0): cv.templatable(cv.int_range(min=0, max=255)),
+            cv.Optional(CONF_CUT_MODE, default=0): cv.templatable(
+                cv.int_range(min=0, max=66)
+            ),
+            cv.Optional(CONF_FEED_LINES, default=0): cv.templatable(
+                cv.int_range(min=0, max=255)
+            ),
         }
     ),
 )
-async def m5stack_printer_cut_paper_action_to_code(config, action_id, template_arg, args):
+async def m5stack_printer_cut_paper_action_to_code(
+    config, action_id, template_arg, args
+):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     templ = await cg.templatable(config[CONF_CUT_MODE], args, cg.uint8)
@@ -135,11 +426,15 @@ async def m5stack_printer_cut_paper_action_to_code(config, action_id, template_a
     cv.Schema(
         {
             cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
-            cv.Required(CONF_ALIGNMENT): cv.templatable(cv.int_range(min=0, max=2)),  # 0=left, 1=center, 2=right
+            cv.Required(CONF_ALIGNMENT): cv.templatable(
+                cv.int_range(min=0, max=2)
+            ),  # 0=left, 1=center, 2=right
         }
     ),
 )
-async def m5stack_printer_set_alignment_action_to_code(config, action_id, template_arg, args):
+async def m5stack_printer_set_alignment_action_to_code(
+    config, action_id, template_arg, args
+):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     templ = await cg.templatable(config[CONF_ALIGNMENT], args, cg.uint8)
@@ -154,13 +449,17 @@ async def m5stack_printer_set_alignment_action_to_code(config, action_id, templa
         {
             cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
             cv.Optional(CONF_BOLD, default=False): cv.templatable(cv.boolean),
-            cv.Optional(CONF_UNDERLINE, default=0): cv.templatable(cv.int_range(min=0, max=2)),
+            cv.Optional(CONF_UNDERLINE, default=0): cv.templatable(
+                cv.int_range(min=0, max=2)
+            ),
             cv.Optional(CONF_DOUBLE_WIDTH, default=False): cv.templatable(cv.boolean),
             cv.Optional(CONF_UPSIDE_DOWN, default=False): cv.templatable(cv.boolean),
         }
     ),
 )
-async def m5stack_printer_set_style_action_to_code(config, action_id, template_arg, args):
+async def m5stack_printer_set_style_action_to_code(
+    config, action_id, template_arg, args
+):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     templ = await cg.templatable(config[CONF_BOLD], args, cg.bool_)
@@ -183,7 +482,9 @@ async def m5stack_printer_set_style_action_to_code(config, action_id, template_a
         }
     ),
 )
-async def m5stack_printer_print_test_page_action_to_code(config, action_id, template_arg, args):
+async def m5stack_printer_print_test_page_action_to_code(
+    config, action_id, template_arg, args
+):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
@@ -197,16 +498,20 @@ async def m5stack_printer_print_test_page_action_to_code(config, action_id, temp
             cv.GenerateID(): cv.use_id(M5StackPrinterDisplay),
             cv.Optional(CONF_SHOW_QR_CODE, default=False): cv.templatable(cv.boolean),
             cv.Optional(CONF_SHOW_BARCODE, default=False): cv.templatable(cv.boolean),
-            cv.Optional(CONF_SHOW_TEXT_STYLES, default=False): cv.templatable(cv.boolean),
+            cv.Optional(CONF_SHOW_TEXT_STYLES, default=False): cv.templatable(
+                cv.boolean
+            ),
             cv.Optional(CONF_SHOW_INVERSE, default=False): cv.templatable(cv.boolean),
             cv.Optional(CONF_SHOW_ROTATION, default=False): cv.templatable(cv.boolean),
         }
     ),
 )
-async def m5stack_printer_run_demo_action_to_code(config, action_id, template_arg, args):
+async def m5stack_printer_run_demo_action_to_code(
+    config, action_id, template_arg, args
+):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
-    
+
     templ = await cg.templatable(config[CONF_SHOW_QR_CODE], args, cg.bool_)
     cg.add(var.set_show_qr_code(templ))
     templ = await cg.templatable(config[CONF_SHOW_BARCODE], args, cg.bool_)
@@ -217,5 +522,5 @@ async def m5stack_printer_run_demo_action_to_code(config, action_id, template_ar
     cg.add(var.set_show_inverse(templ))
     templ = await cg.templatable(config[CONF_SHOW_ROTATION], args, cg.bool_)
     cg.add(var.set_show_rotation(templ))
-    
+
     return var
--- components/m5stack_printer/m5stack_printer.h
+++ components/m5stack_printer/m5stack_printer.h
@@ -16,7 +16,7 @@ namespace m5stack_printer {
 // These values correspond to format 1 (m=0-6) from the datasheet
 enum BarcodeType {
   UPC_A = 0,     // 11-12 characters, digits only
-  UPC_E = 1,     // 11-12 characters, digits only  
+  UPC_E = 1,     // 11-12 characters, digits only
   EAN13 = 2,     // 12-13 characters, digits only (was JAN13)
   EAN8 = 3,      // 7-8 characters, digits only (was JAN8)
   CODE39 = 4,    // Variable length, alphanumeric + special chars
@@ -28,16 +28,16 @@ enum BarcodeType {
 
 /**
  * M5Stack Thermal Printer Component
- * 
+ *
  * This component provides ESC/POS compatible thermal printing functionality
  * for the M5Stack thermal printer module (CSN-A2 based).
- * 
+ *
  * Supported features:
  * - Text printing with font size control (0-7)
  * - QR code printing with error correction
  * - 1D barcode printing (UPC, EAN, CODE39, CODE128, etc.)
  * - Raster image printing via display buffer
- * 
+ *
  * Communication: UART (TTL/RS232) at 9600 baud by default
  * Print width: 58mm (384 dots), 8 dots/mm resolution
  */
@@ -63,13 +63,13 @@ class M5StackPrinterDisplay : public display::DisplayBuffer, public uart::UARTDe
    * @param font_size Font size multiplier (0-7), affects both width and height
    */
   void print_text(std::string text, uint8_t font_size = 0);
-  
+
   /**
    * Print newlines to advance paper
    * @param lines Number of lines to advance (1-255)
    */
   void new_line(uint8_t lines);
-  
+
   /**
    * Print QR code with error correction level L
    * @param data String data to encode (max ~100 characters recommended)
@@ -82,39 +82,39 @@ class M5StackPrinterDisplay : public display::DisplayBuffer, public uart::UARTDe
    * @param type Barcode format (UPC_A, EAN13, CODE128, etc.)
    */
   void print_barcode(std::string barcode, BarcodeType type);
-  
+
   /**
    * Cut paper (full cut)
    * Requires printer with cutting mechanism
    */
   void cut_paper();
-  
+
   /**
    * Cut paper with specified mode
    * @param mode 0=full cut, 1=partial cut, 66=cut and feed
    * @param feed_lines Additional lines to feed before cutting (mode 66 only)
    */
   void cut_paper(uint8_t mode, uint8_t feed_lines = 0);
-  
+
   /**
    * Set text alignment
    * @param alignment 0=left, 1=center, 2=right
    */
   void set_text_alignment(uint8_t alignment);
-  
+
   /**
    * Set line spacing
    * @param spacing Line spacing in dots (0-255)
    */
   void set_line_spacing(uint8_t spacing);
-  
+
   /**
    * Set print density and break time
    * @param density Print density (0-31, affects darkness)
    * @param break_time Break time between dots (0-7)
    */
   void set_print_density(uint8_t density, uint8_t break_time = 0);
-  
+
   /**
    * Set text style modes
    * @param bold Enable bold/emphasized text
@@ -123,55 +123,55 @@ class M5StackPrinterDisplay : public display::DisplayBuffer, public uart::UARTDe
    * @param upside_down Enable upside down text
    */
   void set_text_style(bool bold = false, uint8_t underline = 0, bool double_width = false, bool upside_down = false);
-  
+
   /**
    * Print test page
    * Useful for checking printer functionality
    */
   void print_test_page();
-  
+
   /**
    * Check printer status
    * @return Status byte with paper/voltage/temperature flags
    */
   uint8_t get_printer_status();
-  
+
   /**
    * Enter sleep mode after timeout
    * @param timeout_seconds Time to wait before sleep (0=disable)
    * Useful for battery-powered applications
    */
   void set_sleep_mode(uint16_t timeout_seconds);
-  
+
   /**
    * Set 90-degree clockwise rotation mode
    * @param enable True to enable rotation, false to disable
    */
   void set_90_degree_rotation(bool enable);
-  
+
   /**
    * Set white/black inverse printing mode
    * @param enable True to enable inverse (white text on black), false for normal
    */
   void set_inverse_printing(bool enable);
-  
+
   /**
    * Enable Chinese/Japanese character mode
    * When enabled, processes multi-byte characters for Asian text
    * @param enable True to enable Asian character mode, false for ASCII mode
    */
   void set_chinese_mode(bool enable);
-  
+
   /**
    * Run demo/debug function to showcase all printer capabilities
    * Features Hitchhiker's Guide to the Galaxy themed content
    * @param show_qr_code Print QR code demo (default: false, true when no params)
-   * @param show_barcode Print barcode demo (default: false, true when no params)  
+   * @param show_barcode Print barcode demo (default: false, true when no params)
    * @param show_text_styles Print various text formatting demos (default: false, true when no params)
    * @param show_inverse Print inverse text demo (default: false, true when no params)
    * @param show_rotation Print 90-degree rotated text demo (default: false, true when no params)
    */
-  void run_demo(bool show_qr_code = false, bool show_barcode = false, 
+  void run_demo(bool show_qr_code = false, bool show_barcode = false,
                 bool show_text_styles = false, bool show_inverse = false, bool show_rotation = false);
 
  protected:
@@ -191,7 +191,47 @@ class M5StackPrinterPrintTextAction : public Action<Ts...>, public Parented<M5St
   TEMPLATABLE_VALUE(std::string, text)
   TEMPLATABLE_VALUE(uint8_t, font_size)
 
-  void play(Ts... x) override { this->parent_->print_text(this->text_.value(x...), this->font_size_.value(x...)); }
+  void play(const Ts &...x) override { this->parent_->print_text(this->text_.value(x...), this->font_size_.value(x...)); }
+};
+
+template<typename... Ts>
+class M5StackPrinterPrintQRCodeAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(std::string, qrcode)
+
+  void play(const Ts &...x) override { this->parent_->print_qrcode(this->qrcode_.value(x...)); }
+};
+
+template<typename... Ts>
+class M5StackPrinterPrintBarcodeAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(std::string, barcode)
+  TEMPLATABLE_VALUE(std::string, type)
+
+  void play(Ts... x) override {
+    // Convert string type to BarcodeType enum
+    std::string type_str = this->type_.value(x...);
+    BarcodeType barcode_type = CODE128; // Default
+    if (type_str == "UPC_A") barcode_type = UPC_A;
+    else if (type_str == "UPC_E") barcode_type = UPC_E;
+    else if (type_str == "EAN13") barcode_type = EAN13;
+    else if (type_str == "EAN8") barcode_type = EAN8;
+    else if (type_str == "CODE39") barcode_type = CODE39;
+    else if (type_str == "ITF") barcode_type = ITF;
+    else if (type_str == "CODABAR") barcode_type = CODABAR;
+    else if (type_str == "CODE93") barcode_type = CODE93;
+    else if (type_str == "CODE128") barcode_type = CODE128;
+
+    this->parent_->print_barcode(this->barcode_.value(x...), barcode_type);
+  }
+};
+
+template<typename... Ts>
+class M5StackPrinterNewLineAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(uint8_t, lines)
+
+  void play(const Ts &...x) override { this->parent_->new_line(this->lines_.value(x...)); }
 };
 
 template<typename... Ts>
@@ -200,11 +240,11 @@ class M5StackPrinterCutPaperAction : public Action<Ts...>, public Parented<M5Sta
   TEMPLATABLE_VALUE(uint8_t, cut_mode)
   TEMPLATABLE_VALUE(uint8_t, feed_lines)
 
-  void play(Ts... x) override { 
+  void play(Ts... x) override {
     if (this->cut_mode_.value(x...) == 0 && this->feed_lines_.value(x...) == 0) {
-      this->parent_->cut_paper(); 
+      this->parent_->cut_paper();
     } else {
-      this->parent_->cut_paper(this->cut_mode_.value(x...), this->feed_lines_.value(x...)); 
+      this->parent_->cut_paper(this->cut_mode_.value(x...), this->feed_lines_.value(x...));
     }
   }
 };
@@ -214,7 +254,7 @@ class M5StackPrinterSetAlignmentAction : public Action<Ts...>, public Parented<M
  public:
   TEMPLATABLE_VALUE(uint8_t, alignment)
 
-  void play(Ts... x) override { this->parent_->set_text_alignment(this->alignment_.value(x...)); }
+  void play(const Ts &...x) override { this->parent_->set_text_alignment(this->alignment_.value(x...)); }
 };
 
 template<typename... Ts>
@@ -225,20 +265,98 @@ class M5StackPrinterSetStyleAction : public Action<Ts...>, public Parented<M5Sta
   TEMPLATABLE_VALUE(bool, double_width)
   TEMPLATABLE_VALUE(bool, upside_down)
 
-  void play(Ts... x) override { 
+  void play(Ts... x) override {
     this->parent_->set_text_style(
-      this->bold_.value(x...), 
-      this->underline_.value(x...), 
-      this->double_width_.value(x...), 
+      this->bold_.value(x...),
+      this->underline_.value(x...),
+      this->double_width_.value(x...),
       this->upside_down_.value(x...)
-    ); 
+    );
+  }
+};
+
+template<typename... Ts>
+class M5StackPrinterSet90DegreeRotationAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(bool, enable)
+
+  void play(const Ts &...x) override { this->parent_->set_90_degree_rotation(this->enable_.value(x...)); }
+};
+
+template<typename... Ts>
+class M5StackPrinterSetInversePrintingAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(bool, enable)
+
+  void play(const Ts &...x) override { this->parent_->set_inverse_printing(this->enable_.value(x...)); }
+};
+
+template<typename... Ts>
+class M5StackPrinterSetChineseModeAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(bool, enable)
+
+  void play(const Ts &...x) override { this->parent_->set_chinese_mode(this->enable_.value(x...)); }
+};
+
+template<typename... Ts>
+class M5StackPrinterSetLineSpacingAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(uint8_t, spacing)
+
+  void play(const Ts &...x) override { this->parent_->set_line_spacing(this->spacing_.value(x...)); }
+};
+
+template<typename... Ts>
+class M5StackPrinterSetPrintDensityAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(uint8_t, density)
+  TEMPLATABLE_VALUE(uint8_t, break_time)
+
+  void play(const Ts &...x) override {
+    this->parent_->set_print_density(this->density_.value(x...), this->break_time_.value(x...));
+  }
+};
+
+template<typename... Ts>
+class M5StackPrinterSetTabPositionsAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(std::string, positions)
+
+  void play(Ts... x) override {
+    // Parse comma or space separated positions and call set_tab_positions
+    // For now, just log - the actual implementation would parse the string
+    ESP_LOGD("m5stack_printer", "Set tab positions: %s", this->positions_.value(x...).c_str());
+    // TODO: Implement tab position parsing and setting
+  }
+};
+
+template<typename... Ts>
+class M5StackPrinterHorizontalTabAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  void play(const Ts &...x) override {
+    // Send horizontal tab character (0x09)
+    this->parent_->write_byte(0x09);
+  }
+};
+
+template<typename... Ts>
+class M5StackPrinterSetHorizontalPositionAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
+ public:
+  TEMPLATABLE_VALUE(uint16_t, position)
+
+  void play(const Ts &...x) override {
+    // Send ESC $ nL nH command to set absolute horizontal position
+    uint16_t pos = this->position_.value(x...);
+    uint8_t cmd[] = {0x1B, '$', static_cast<uint8_t>(pos & 0xFF), static_cast<uint8_t>(pos >> 8)};
+    this->parent_->write_array(cmd, 4);
   }
 };
 
 template<typename... Ts>
 class M5StackPrinterPrintTestPageAction : public Action<Ts...>, public Parented<M5StackPrinterDisplay> {
  public:
-  void play(Ts... x) override { this->parent_->print_test_page(); }
+  void play(const Ts &...x) override { this->parent_->print_test_page(); }
 };
 
 template<typename... Ts>
@@ -250,13 +368,13 @@ class M5StackPrinterRunDemoAction : public Action<Ts...>, public Parented<M5Stac
   TEMPLATABLE_VALUE(bool, show_inverse)
   TEMPLATABLE_VALUE(bool, show_rotation)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     bool qr = this->show_qr_code_.value(x...);
     bool bc = this->show_barcode_.value(x...);
     bool txt = this->show_text_styles_.value(x...);
     bool inv = this->show_inverse_.value(x...);
     bool rot = this->show_rotation_.value(x...);
-    
+
     // If no specific flags set, run full demo
     if (!qr && !bc && !txt && !inv && !rot) {
       this->parent_->run_demo(true, true, true, true, true);
